55ece7ce2d66fbbe43fb1700e07a273b2480f387
S. Elliott Johnson <sejohnson@torchcloudconsulting.com>
2022-05-03 01:27:46 -0600

    feat: Give some options in hooks.ts

--- src/hooks.ts
+++ src/hooks.ts
@@ -2,6 +2,15 @@ import type { Handle, HandleError } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
 	return await resolve(event);
+	//Try commenting out the above line and uncommenting the below
+	// try {
+	// 	return await resolve(event);
+	// } catch {
+	// 	console.log('caught');
+	// 	// Then comment out this line and look what happens to the error code
+	// 	// rendered on the error page
+	// 	throw new Error('haha, caught');
+	// }
 };
 
 export const handleError: HandleError = async ({ error, event }) => {
